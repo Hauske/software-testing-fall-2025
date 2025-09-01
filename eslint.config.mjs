d04046337875f1568a338d0ce1fd4894f74ac306
Hauske <dany.rams@hotmail.com>
2025-09-01 12:03:37 -0600

    feat: static analysis
    Test of rebase

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+import prettier from "eslint-plugin-prettier";
+import globals from "globals";
+
+export default [
+  {
+    files: ["**/*.js"],
+    ignores: [".github/**", "node_modules/**"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    },
+    plugins: {
+      prettier
+    },
+    rules: {
+      // ESLint recommended rules
+      "no-unused-vars": "warn",
+      "no-console": "off",
+
+      // Prettier integration
+      "prettier/prettier": [
+        "error",
+        {
+          semi: true,
+          singleQuote: true,
+          trailingComma: "es5"
+        }
+      ]
+    }
+  }
+];
